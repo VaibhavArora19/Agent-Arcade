f595112560d76195518080c23bade185fde6abac
Vaibhav Arora <aroravaibhav817@gmail.com>
2025-01-31 23:19:42 +0530

    added agents summary

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,11 @@
+import AgentSummary from "@/components/Dashboard/agent-summary";
 import DashboardHeading from "@/components/Dashboard/dashboard-heading";
 
 export default function Home() {
   return (
     <div>
       <DashboardHeading />
+      <AgentSummary />
     </div>
   );
 }
---
+++ frontend/components/Dashboard/agent-summary.tsx
@@ -0,0 +1,45 @@
+import { Button } from "../ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { GoPeople } from "react-icons/go";
+
+const DUMMY_DATA = [
+  {
+    title: "Total Characters",
+    value: 2,
+    change: "+12%",
+    icon: <GoPeople />,
+  },
+  {
+    title: "Total Games",
+    value: 1,
+    change: "+6%",
+    icon: <GoPeople />,
+  },
+];
+
+const AgentSummary = () => {
+  return (
+    <div className="px-8">
+      <div className=" flex gap-8">
+        {DUMMY_DATA.map((item) => {
+          return (
+            <Card key={item.title} className="w-[350px]">
+              <CardHeader>
+                <CardTitle>{item.title}</CardTitle>
+              </CardHeader>
+              <CardContent className="flex justify-between">
+                <div className="flex gap-4">
+                  <p className="text-4xl font-semibold">{item.value}</p>
+                  <Button>{item.change}</Button>
+                </div>
+                <div>{item.icon}</div>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default AgentSummary;
